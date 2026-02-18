b7e6bb1db4d18e98652ac4cf9839803ee7649139
aryangupta0810 <aryan.gupta_cs.aiml24@gla.ac.in>
2026-02-18 08:35:28 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
 #include <Arduino.h>
+#define ldrpin A0
 void setup() {
+    Serial.begin(9600);
+    pinMode(ldrpin, INPUT);
+    Serial.println("LDRPin setup");
+
 // write your initialization code here
 }
 
 void loop() {
+    int ldrRaw = analogRead(ldrpin);
+    int lightPerecentage = map(ldrRaw, 0, 1023, 0, 100);
+    Serial.println("LDRRaw");
+    Serial.println(ldrRaw);
+    Serial.println(" | light: ");
+    Serial.println(lightPerecentage);
+    Serial.println("%");
+    delay(500);
 // write your code here
 }
